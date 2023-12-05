23803c9a9f2aff5014b97d22bd34b6a18e6f1c49
Frank Luan <lsf@berkeley.edu>
2023-12-05 17:07:52 +0000

    Make sure the shuffle/no shuffle versions task graphs are the same

--- nanobenchmarks/skew.py
+++ nanobenchmarks/skew.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 import numpy as np
@@ -8,27 +9,37 @@ DATA_SIZE = 1000 * 200
 
 
 def gen_data(row):
-    i = row["id"]
+    i = row["item"]
     print(f"gen_data {i}: {DATA_SIZE * i:,}")
-    data = np.random.rand(DATA_SIZE * i)
-    for _ in range(i):
-        data += np.random.rand(DATA_SIZE * i)
-    return {"data": data, **row}
+    data = sum(np.random.rand(DATA_SIZE * i) for _ in range(i + 1))
+    return {"data": data, "id": i}
 
 
 def memory_blowup(row, *, blowup: int):
     i = row["id"]
     x = row["data"]
     print(f"memory_blowup {i}: {x.nbytes:,} * {blowup} = {x.nbytes * blowup:,}")
-    return {"data": np.concatenate([x + np.random.rand(DATA_SIZE * i * i) for _ in range(blowup)])}
+    return {
+        "data": np.concatenate(
+            [x + np.random.rand(DATA_SIZE * i * i) for _ in range(blowup)]
+        )
+    }
 
 
-def run_experiment(*, blowup: int = 0, parallelism: int = -1, size: int = -1, random_shuffle: bool = False):
+def run_experiment(
+    *,
+    blowup: int = 0,
+    parallelism: int = -1,
+    size: int = -1,
+    random_shuffle: bool = False,
+):
     start = time.perf_counter()
 
-    ds = ray.data.range(size, parallelism=parallelism)
+    items = list(range(size))
     if random_shuffle:
-        ds = ds.random_shuffle()
+        random.shuffle(items)
+
+    ds = ray.data.from_items(items, parallelism=parallelism)
     ds = ds.map(gen_data)
     if blowup > 0:
         ds = ds.map(memory_blowup, fn_kwargs={"blowup": blowup})
