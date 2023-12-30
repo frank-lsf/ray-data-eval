e7b6e5a9d71224689a2816ba39bc80fe241e0f4b
ronyw7 <yifengwang@berkeley.edu>
2023-12-30 12:26:31 +0000

    recorded uniform_flink results

--- nanobenchmarks/uniform_flink.py
+++ nanobenchmarks/uniform_flink.py
@@ -7,16 +7,13 @@ from pyflink.common import Configuration
 DATA_SIZE_BYTES = 1000 * 1000 * 100  # 100 MB
 TIME_BASIS = 0.1  # How many seconds should time_factor=1 take
 
-# TODO: READ UP ON FLINK BACKPRESSURE POLICY
-
 def memory_blowup(x, time_factor):
     data = b"1" * DATA_SIZE_BYTES
     time.sleep(TIME_BASIS * time_factor)
     return (data, x)
 
-
 def memory_shrink(item, time_factor):
-    data, idx = item
+    data, x = item
     time.sleep(TIME_BASIS * time_factor)
     return len(data)
 
@@ -36,7 +33,9 @@ def run_experiment(
     ds = ds.map(
         lambda x: memory_blowup(x, producer_time),
         output_type=Types.TUPLE([Types.PICKLED_BYTE_ARRAY(), Types.INT()]),
-    ).map(lambda x: memory_shrink(x, consumer_time), output_type=Types.LONG())
+    ).disable_chaining()
+
+    ds = ds.map(lambda x: memory_shrink(x, consumer_time), output_type=Types.LONG())
 
     result = ds.execute_and_collect()
     total_length = sum(result)
@@ -47,25 +46,31 @@ def run_experiment(
     print(f"Time: {end - start:.4f}s")
 
 
-
 def main():
-    # Specify `THREAD` mode
+    # Using `THREAD` mode
     config = Configuration()
     config.set_string("python.execution-mode", "thread")
     env = StreamExecutionEnvironment.get_execution_environment(config)
 
-    # Default `PROCESS` mode
-    # env = StreamExecutionEnvironment.get_execution_environment(config)
+    # Using default `PROCESS` mode
+    # env = StreamExecutionEnvironment.get_execution_environment()
 
     config = {
-        "parallelism": 100,
-        "num_parts": 100,  # Number of parts/items
-        "producer_time": 1,  # Simulated processing time in memory_blowup
-        "consumer_time": 9 # Simulated processing time in memory_shrink
+        "parallelism": 20,
+        "num_parts": 100,  
+        "producer_time": 1,  
+        "consumer_time": 9, 
     }
-    env.set_parallelism(config["parallelism"])  # Example parallelism
+    env.set_parallelism(config["parallelism"])  
 
-    run_experiment(env, config["parallelism"], config["num_parts"], config["producer_time"], config["consumer_time"])
+    run_experiment(
+        env,
+        config["parallelism"],
+        config["num_parts"],
+        config["producer_time"],
+        config["consumer_time"],
+    )
+
 
 if __name__ == "__main__":
     main()
