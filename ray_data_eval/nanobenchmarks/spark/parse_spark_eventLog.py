0b914f5fa81e797906d64cd0d331d778197f11c1
Charlotte Lin <charlottelinaus@gmail.com>
2024-01-19 17:53:05 +0000

    Update spark event log parse to follow chrome tracing plot data schema

--- ray_data_eval/nanobenchmarks/spark/parse_spark_eventLog.py
+++ ray_data_eval/nanobenchmarks/spark/parse_spark_eventLog.py
@@ -2,51 +2,50 @@ import os
 import json
 
 
-def spark_timeline():
+def make_trace_event_spark():
     # logs/spark-events/app-20240109133439-0022
+    app_id = "app-20240119050536-0174"
 
     log_directory = os.getenv("SPARK_EVENTS_PATH")
-    app_id = "app-20240115141650-0146"
+    output_directory = os.getenv("SPARK_TRACE_EVENT_PATH")
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
 
-    job_info_dict = {}
+    events = []
 
     file = os.path.join(log_directory, app_id)
     with open(file, "r") as f:
         for line in f:
             event = json.loads(line)
 
-            if event["Event"] == "SparkListenerJobStart":
-                job_id = event["Job ID"]
-                job_info_dict[job_id] = {
-                    "start_time": (event["Submission Time"]),
-                    "tasks": [],
-                }
-
-            elif event["Event"] == "SparkListenerTaskEnd":
-                task_info = event["Task End Reason"]
+            if event["Event"] == "SparkListenerTaskEnd":
                 start_time = event["Task Info"]["Launch Time"]
                 end_time = event["Task Info"]["Finish Time"]
                 task_id = event["Task Info"]["Task ID"]
+                executor_id = event["Task Info"]["Executor ID"]
+                host = event["Task Info"]["Host"]
 
-                job_info_dict[job_id]["tasks"].append(
-                    {
-                        "task_id": task_id,
-                        "start_time": start_time,
-                        "end_time": end_time,
-                        "task_info": task_info,
-                    }
-                )
+                event = {
+                    "cat": f"task:{task_id}",
+                    "name": f"task:{task_id}",
+                    "pid": host,
+                    "tid": f"worker:{executor_id}",
+                    "ts": start_time,  # start time (ms)
+                    "dur": (end_time - start_time),  # duration (ms)
+                    "ph": "X",
+                    "args": {},
+                }
 
-    for job_id, job_info in job_info_dict.items():
-        print(f"Job ID: {job_id}, Start Time: {job_info['start_time']}")
-        for task_info in job_info["tasks"]:
-            print(
-                f"  Task ID: {task_info['task_id']}, Start Time: {task_info['start_time']}, End Time: {task_info['end_time']}, Task Info: {task_info['task_info']}"
-            )
+                events.append(event)
+
+    output_file = os.path.join(output_directory, f"timeline_{app_id}.json")
+    with open(output_file, "w") as output_f:
+        json.dump(events, output_f, indent=2)
+        print(f"Output saved to: {output_file}")
 
 
 def main():
-    spark_timeline()
+    make_trace_event_spark()
 
 
 if __name__ == "__main__":
