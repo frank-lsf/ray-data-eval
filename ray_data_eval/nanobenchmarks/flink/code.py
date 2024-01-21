a594e1ba1f5270a275561988578e98285c0ba03c
ronyw7 <yifengwang@berkeley.edu>
2024-01-21 22:04:53 +0000

    completed logging and parsing

--- ray_data_eval/nanobenchmarks/flink/code.py
+++ ray_data_eval/nanobenchmarks/flink/code.py
@@ -1,5 +1,6 @@
 import time
 import logging
+import json
 
 from pyflink.common.typeinfo import Types
 from pyflink.datastream import StreamExecutionEnvironment, MapFunction, RuntimeContext
@@ -7,41 +8,21 @@ from pyflink.common import Configuration
 
 from ray_data_eval.common.types import SchedulingProblem, test_problem
 
+from parse import logging_helper
+
 DATA_SIZE_BYTES = 1000 * 1000 * 100  # 100 MB
 # DATA_SIZE_BYTES = 1000 * 1000 * 100  # 10 MB
 # DATA_SIZE_BYTES = 1
 TIME_UNIT = 1  # seconds
 
-# def producer(item, cfg: SchedulingProblem):
-#     producer_start = time.time()
-#     data = b"1" * (DATA_SIZE_BYTES * cfg.producer_output_size[item])
-#     time.sleep(TIME_UNIT * cfg.producer_time[item])
-
-#     producer_end = time.time()
-
-#     logging.warning(f"Producer started at {producer_start} and finished at {producer_end}")
-#     return (data, item)
-
-
-# def consumer(item, cfg: SchedulingProblem):
-#     consumer_start = time.time()
-#     data, i = item
-#     time.sleep(TIME_UNIT * cfg.consumer_time[i])
-
-#     consumer_end = time.time()
-#     logging.warning(f"Consumer started at {consumer_start} and finished at {consumer_end}")
-#     return len(data)
-
 
 class Producer(MapFunction):
     def __init__(self, cfg):
         self.cfg = cfg
-        self.task_name = None
-        self.task_index = None
+        self.task_info = None
 
     def open(self, runtime_context: RuntimeContext):
-        self.task_name = runtime_context.get_task_name()
-        self.task_index = runtime_context.get_task_name_with_subtasks()
+        self.task_info = runtime_context.get_task_name_with_subtasks()
 
     def map(self, item):
         producer_start = time.time()
@@ -49,20 +30,27 @@ class Producer(MapFunction):
         time.sleep(TIME_UNIT * self.cfg.producer_time[item])
         producer_end = time.time()
 
-        logging.warning(
-            f"Producer {self.task_name} - {self.task_index} started at {producer_start} and finished at {producer_end}"
-        )
+        log = {
+            "cat": "producer:" + str(self.task_info),
+            "name": "producer:" + str(self.task_info),
+            "pid": "",
+            "tid": "",
+            "ts": f"{producer_start * 1e6:.0f}",  # time is in microseconds
+            "dur": f"{producer_end * 1e6 - producer_start * 1e6:.0f}",
+            "ph": "X",
+            "args": {},
+        }
+        logging.warning(json.dumps(log))
         return (data, item)
 
 
 class Consumer(MapFunction):
     def __init__(self, cfg):
         self.cfg = cfg
-        self.task_name = None
+        self.task_info = None
 
     def open(self, runtime_context: RuntimeContext):
-        self.task_name = runtime_context.get_task_name()
-        self.task_index = runtime_context.get_task_name_with_subtasks()
+        self.task_info = runtime_context.get_task_name_with_subtasks()
 
     def map(self, item):
         consumer_start = time.time()
@@ -70,9 +58,17 @@ class Consumer(MapFunction):
         time.sleep(TIME_UNIT * self.cfg.consumer_time[i])
         consumer_end = time.time()
 
-        logging.warning(
-            f"Consumer {self.task_name} - {self.task_index} started at {consumer_start} and finished at {consumer_end}"
-        )
+        log = {
+            "cat": "consumer:" + str(self.task_info),
+            "name": "consumer:" + str(self.task_info),
+            "pid": "",
+            "tid": "",
+            "ts": f"{consumer_start * 1e6:.0f}",  # time is in microseconds
+            "dur": f"{consumer_end * 1e6 - consumer_start * 1e6:.0f}",
+            "ph": "X",
+            "args": {},
+        }
+        logging.warning(json.dumps(log))
         return len(data)
 
 
@@ -84,11 +80,6 @@ def run_flink(env, cfg: SchedulingProblem):
 
     items = list(range(cfg.num_producers))
     ds = env.from_collection(items, type_info=Types.INT())
-
-    # ds = ds.map(
-    #     lambda x: producer(x, cfg),
-    #     output_type=Types.TUPLE([Types.PICKLED_BYTE_ARRAY(), Types.INT()]),
-    # ).disable_chaining()
 
     producer = Producer(cfg)
     consumer = Consumer(cfg)
---
+++ ray_data_eval/nanobenchmarks/flink/parse.py
@@ -0,0 +1,51 @@
+import requests
+import json
+
+FLINK_DASHBOARD = 'http://localhost:8082'
+
+def get_job_id(base_url=FLINK_DASHBOARD):
+    url = f"{base_url}/jobs"
+    response = requests.get(url)
+    data = response.json()
+    return data['jobs'][0]['id']
+
+JOB_ID = get_job_id()
+
+def get_taskmanager_ids(base_url=FLINK_DASHBOARD):
+    url = f"{base_url}/taskmanagers"
+    response = requests.get(url)
+    data = response.json()
+    return [tm['id'] for tm in data['taskmanagers']]
+
+def get_taskmanager_stdout(tm_id, base_url=FLINK_DASHBOARD):
+    url = f"{base_url}/taskmanagers/{tm_id}/stdout"
+    response = requests.get(url)
+    return response.text
+
+def parse_stdout_to_json(stdout, tm_id):
+    log_entries = stdout.split('\n')
+    parsed = []
+    for entry in log_entries[:-1]:
+        json_obj = json.loads(entry[len("WARNING:root:"):])
+
+        json_obj['pid'] = JOB_ID
+        json_obj['tid'] = 'taskmanager:' + tm_id
+        parsed.append(json_obj)
+
+    return parsed
+
+def main():
+    out = []
+    taskmanager_ids = get_taskmanager_ids()
+
+    for tm_id in taskmanager_ids:
+        stdout = get_taskmanager_stdout(tm_id)
+        parsed_logs = parse_stdout_to_json(stdout, tm_id)
+        out.extend(parsed_logs)
+    
+    with open('data.json', 'w') as f:
+        json.dump(out, f)
+
+if __name__ == "__main__":
+    main()
+    
