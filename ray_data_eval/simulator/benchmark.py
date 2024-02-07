84870892452ec2ae14fa5c553bc7ef628920fd8f
MaoZiming <ziming.mao@yale.edu>
2024-02-06 17:22:26 -0800

    refactor

--- ray_data_eval/simulator/benchmark.py
+++ ray_data_eval/simulator/benchmark.py
@@ -12,24 +12,6 @@ from ray_data_eval.simulator.policies import (  # noqa F401
 logging.disable(logging.CRITICAL)
 
 
-def test_scheduling_policy(problem: SchedulingProblem, policy: SchedulingPolicy) -> bool:
-    env = ExecutionEnvironment(
-        num_executors=problem.num_execution_slots,
-        buffer_size=problem.buffer_size_limit,
-        tasks=problem.tasks,
-        scheduling_policy=policy,
-    )
-
-    for tick in range(problem.time_limit):
-        logging.info("-" * 60)
-        logging.info(f"Tick {tick}")
-        env.tick()
-
-    env.print_timeline()
-    ret = env.check_all_tasks_finished()
-    logging.info(f"All tasks finished? {ret}")
-
-
 def main():
     for problem in problems:
         print("Problem:", problem.name)
