53fc1395d2667646e20090db585aa0870ed07611
MaoZiming <ziming.mao@yale.edu>
2024-02-28 13:14:36 -0800

    updated e2e_problem

--- ray_data_eval/simulator/test.py
+++ ray_data_eval/simulator/test.py
@@ -48,11 +48,11 @@ def test_scheduling_policy(problem: SchedulingProblem, policy: SchedulingPolicy)
 
 
 def main():
-    problem = e2e_problem2
-    # policy = GreedyPolicy(problem)
+    problem = e2e_problem
+    policy = GreedyPolicy(problem)
     # policy = GreedyWithBufferPolicy(problem)
     # policy = GreedyOracleProducerFirstPolicy(problem)
-    policy = ConcurrencyCapPolicy(problem)
+    # policy = DelayPolicy(problem)
     test_scheduling_policy(problem, policy)
 
 
