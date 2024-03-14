91e371a8a99bb254e99e621941c60d8ad72b45bc
MaoZiming <ziming.mao@yale.edu>
2024-03-14 17:59:41 +0000

    training.out

--- ray_data_eval/image_training/e2e_training/run_script.sh
+++ ray_data_eval/image_training/e2e_training/run_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup python main.py -a resnet50 -b 128 ~/imagenet/ILSVRC/Data/CLS-LOC > output.log 2>&1 &
+nohup python main.py -a resnet50 -b 128 ~/imagenet/ILSVRC/Data/CLS-LOC > training.out 2>&1 &
