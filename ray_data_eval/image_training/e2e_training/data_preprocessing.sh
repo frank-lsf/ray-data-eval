2dcc569adc63fe1f936474233ef14c7d650ea796
MaoZiming <ziming.mao@yale.edu>
2024-03-15 03:18:37 +0000

    added trianing_v100_4 results

--- ray_data_eval/image_training/e2e_training/data_preprocessing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Download from s3 bucket into a folder ~/imagenet
-
-cd ~/imagenet/ILSVRC/Data/val
-
-# get script from soumith and run; this script creates all class directories and moves images into corresponding directories
-wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash
