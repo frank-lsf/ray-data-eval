839485c080a39885bc5e51510a78e4c2d590d19b
Frank Luan <lsf@berkeley.edu>
2024-04-05 01:30:26 +0000

    simple pipeline with videomae

--- ray_data_eval/video_inference/simple_pipeline.py
+++ ray_data_eval/video_inference/simple_pipeline.py
@@ -5,35 +5,23 @@ import os
 import time
 
 import humanize
-from pytorchvideo.data.encoded_video import EncodedVideoPyAV
-from pytorchvideo.transforms import (
-    ApplyTransformToKey,
-    ShortSideScale,
-    UniformTemporalSubsample,
-)
+import numpy as np
 import torch
-from torchvision.transforms import Compose, Lambda
-from torchvision.transforms._transforms_video import (
-    CenterCropVideo,
-    NormalizeVideo,
+import transformers
+from transformers import (
+    VideoMAEImageProcessor,
+    VideoMAEForVideoClassification,
 )
 
 DEVICE = "cuda"
-SIDE_SIZE = 256
-MEAN = [0.45, 0.45, 0.45]
-STD = [0.225, 0.225, 0.225]
-CROP_SIZE = 256
-NUM_FRAMES = 32
-SAMPLING_RATE = 2
-FPS = 30
-CLIP_DURATION = (NUM_FRAMES * SAMPLING_RATE) / FPS
-START_SEC = 0
-END_SEC = START_SEC + CLIP_DURATION
+MODEL_ID = "MCG-NJU/videomae-base-finetuned-kinetics"
+IMAGE_SIZE = 224
+NUM_FRAMES = 16
 
 DATA_PATH = "/mnt/data/ray-data-eval/kinetics"
 INPUT_PATH = "/mnt/data/ray-data-eval/kinetics/Kinetics700-2020-test"
 
-ModelInputType = tuple[torch.Tensor, torch.Tensor]
+ModelInputType = torch.Tensor
 
 
 def tensor_size(t: torch.Tensor) -> str:
@@ -51,114 +39,61 @@ def timeit(func):
     return wrapper
 
 
-class PackPathway(torch.nn.Module):
-    """
-    Transform for converting video frames as a list of tensors.
-    """
-
-    SLOWFAST_ALPHA = 4
-
-    def forward(self, frames: torch.Tensor) -> ModelInputType:
-        fast_pathway = frames
-        # Perform temporal sampling from the fast pathway.
-        slow_pathway = torch.index_select(
-            frames,
-            1,
-            torch.linspace(0, frames.shape[1] - 1, frames.shape[1] // self.SLOWFAST_ALPHA).long(),
-        )
-        return slow_pathway, fast_pathway
-
-
-transform = ApplyTransformToKey(
-    key="video",
-    transform=Compose(
-        [
-            UniformTemporalSubsample(NUM_FRAMES),
-            Lambda(lambda x: x / 255.0),
-            NormalizeVideo(MEAN, STD),
-            ShortSideScale(size=SIDE_SIZE),
-            CenterCropVideo(CROP_SIZE),
-            PackPathway(),
-        ]
-    ),
-)
-
-
 @timeit
 def load_file(file_path: str) -> io.BytesIO:
     with open(file_path, "rb") as f:
         return io.BytesIO(f.read())
 
 
-@timeit
-def preprocess_video(video_bytes: io.BytesIO) -> torch.Tensor:
-    print("video bytes", humanize.naturalsize(video_bytes.getbuffer().nbytes))
-    video = EncodedVideoPyAV(video_bytes)
-    video_data = video.get_clip(start_sec=START_SEC, end_sec=CLIP_DURATION)
-    print("decoded video size", tensor_size(video_data["video"]))
-    video_data = transform(video_data)
-    inputs = video_data["video"]
-    return inputs
-
-
-def _get_kinetics_classnames() -> dict[str, str]:
-    with open(f"{DATA_PATH}/kinetics_classnames.json", "r") as f:
-        kinetics_classnames = json.load(f)
-
-    # Create an id to label name mapping
-    kinetics_id_to_classname = {}
-    for k, v in kinetics_classnames.items():
-        kinetics_id_to_classname[v] = str(k).replace('"', "")
-
-    return kinetics_id_to_classname
-
-
 class Classifier:
     def __init__(self):
         start_time = time.time()
-        self.model = (
-            torch.hub.load("facebookresearch/pytorchvideo", "slowfast_r50", pretrained=True)
-            .eval()
-            .to(DEVICE)
-        )
-        self.post_act = torch.nn.Softmax(dim=1)
-        self.kinetics_id_to_classname = _get_kinetics_classnames()
+        self.model = VideoMAEForVideoClassification.from_pretrained(MODEL_ID).eval().to(DEVICE)
         print(f"Time to initialize model: {time.time() - start_time}")
 
     @timeit
-    def __call__(self, batch: ModelInputType) -> list:
-        for tensor in batch:
-            print(tensor_size(tensor))
-        batch = [tensor.to(DEVICE) for tensor in batch]
-        preds = self.model(batch)
-        preds = self.post_act(preds)
-        topk_indices = preds.topk(k=5, dim=1).indices
-        batch_pred_class_names = []
-        for pred_classes in topk_indices:
-            pred_class_names = [self.kinetics_id_to_classname[int(i)] for i in pred_classes]
-            pred_class_names = ", ".join(pred_class_names)
-            print(pred_class_names)
-            batch_pred_class_names.append(pred_class_names)
-        return batch_pred_class_names
+    @torch.no_grad
+    def __call__(self, model_input: ModelInputType) -> list:
+        print(model_input.shape)
+        model_input = model_input.to(DEVICE)
+        model_output = self.model(pixel_values=model_input)
+        logits = model_output.logits
+        preds = logits.argmax(-1)
+        for pred in preds:
+            print("Predicted class:", self.model.config.id2label[pred.item()])
+
+
+def read_and_decode_video(file_path: str) -> np.ndarray:
+    from decord import VideoReader
+
+    vr = VideoReader(file_path, num_threads=1, width=IMAGE_SIZE, height=IMAGE_SIZE)
+    frames = vr.get_batch(range(NUM_FRAMES)).asnumpy()
+    frames = list(frames)
+    return frames
 
 
 class SimpleIterator:
     def __init__(self):
-        self.files = os.listdir(INPUT_PATH)
+        self.file_paths = os.listdir(INPUT_PATH)
         self.file_index = 0
+        self.processor = VideoMAEImageProcessor.from_pretrained(MODEL_ID)
 
     def __iter__(self):
         return self
 
+    def _preprocess(self, video: np.ndarray) -> ModelInputType:
+        ret = self.processor(video, return_tensors="pt")
+        return ret.data["pixel_values"]
+
+    @timeit
     def __next__(self) -> ModelInputType:
-        if self.file_index >= len(self.files):
+        if self.file_index >= len(self.file_paths):
             raise StopIteration
-        file = self.files[self.file_index]
-        file_path = os.path.join(INPUT_PATH, file)
-        video_bytes = load_file(file_path)
-        model_input = preprocess_video(video_bytes)
+        file_path = os.path.join(INPUT_PATH, self.file_paths[self.file_index])
+        video = read_and_decode_video(file_path)
+        result = self._preprocess(video)
         self.file_index += 1
-        return model_input
+        return result
 
 
 class BatchIterator:
@@ -171,23 +106,26 @@ class BatchIterator:
 
     @timeit
     def __next__(self) -> ModelInputType:
-        tensor_lists = [], []
+        batch = []
         for _ in range(self.batch_size):
-            slow, fast = next(self._iter)
-            tensor_lists[0].append(slow)
-            tensor_lists[1].append(fast)
-        return [torch.stack(tensor_list) for tensor_list in tensor_lists]
+            try:
+                batch.append(next(self._iter))
+            except StopIteration:
+                break
+        if not batch:
+            raise StopIteration
+        return torch.cat(batch, dim=0)
 
 
 def main():
     n_batches = 0
-    batch_size = 8
+    batch_size = 4
     classifier = Classifier()
-    batch_iterator = BatchIterator(batch_size=batch_size)
-    for batch in batch_iterator:
+    iterator = BatchIterator(batch_size=batch_size)
+    for batch in iterator:
         classifier(batch)
         n_batches += 1
-        if n_batches >= 2:
+        if n_batches >= 3:
             break
 
 
---
+++ ray_data_eval/video_inference/videomae.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+import transformers
+from transformers import (
+    VideoMAEImageProcessor,
+    VideoMAEForVideoClassification,
+)
+import torch
+
+DEVICE = "cuda"
+MODEL_ID = "MCG-NJU/videomae-base-finetuned-kinetics"
+DATA_DIR = "/mnt/data/ray-data-eval/kinetics"
+video_path = f"{DATA_DIR}/archery.mp4"
+
+print(DEVICE)
+processor = VideoMAEImageProcessor.from_pretrained(MODEL_ID)
+model = VideoMAEForVideoClassification.from_pretrained(MODEL_ID)
+model = model.eval().to(DEVICE)
+print("model ok")
+
+from decord import VideoReader
+
+# video clip consists of 300 frames (10 seconds at 30 FPS)
+vr = VideoReader(video_path, num_threads=1)
+
+
+def sample_frame_indices(clip_len, frame_sample_rate, seg_len):
+    converted_len = int(clip_len * frame_sample_rate)
+    end_idx = np.random.randint(converted_len, seg_len)
+    str_idx = end_idx - converted_len
+    index = np.linspace(str_idx, end_idx, num=clip_len)
+    index = np.clip(index, str_idx, end_idx - 1).astype(np.int64)
+    return index
+
+
+vr.seek(0)
+index = sample_frame_indices(clip_len=16, frame_sample_rate=4, seg_len=len(vr))
+video = vr.get_batch(index).asnumpy()  # [nframes, height, width, channels]
+video = list(video)  # as a list of frames
+
+inputs = processor(video, return_tensors="pt").to(DEVICE)
+
+with torch.no_grad():
+    print(inputs["pixel_values"].shape)
+    outputs = model(**inputs)
+    logits = outputs.logits
+
+predicted_class_idx = logits.argmax(-1).item()
+print("Predicted class:", model.config.id2label[predicted_class_idx])
