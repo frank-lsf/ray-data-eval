dc22a01596f5eb451e7c92c1d3c2c99c0d6639a7
Ron Yifeng Wang <78146401+ronyw7@users.noreply.github.com>
2024-04-19 20:00:10 -0700

    Finish a working pipeline for video inference (#42)
    * Finish a working pipeline for video inference
    
    * Clean code

--- ray_data_eval/video_inference/ray_data_pipeline.py
+++ ray_data_eval/video_inference/ray_data_pipeline.py
@@ -1,3 +1,5 @@
+import argparse
+import csv
 import functools
 import io
 import time
@@ -6,22 +8,46 @@ import humanize
 import numpy as np
 import ray
 from ray.data.block import DataBatch
-from ray._private import profiling
+
 import torch
 from transformers import (
     VideoMAEImageProcessor,
     VideoMAEForVideoClassification,
 )
 
+
+from ray_data_pipeline_helpers import ChromeTracer, append_gpu_timeline, download_train_directories
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-s",
+    "--source",
+    default="local",
+    help="local or S3",
+)
+
+args = parser.parse_args()
+
 DEVICE = "cuda"
-MODEL_ID = "MCG-NJU/videomae-huge-finetuned-kinetics"
+MODEL_ID = "MCG-NJU/videomae-base-finetuned-kinetics"
 IMAGE_SIZE = 224
 NUM_FRAMES = 16
 MODEL_INPUT_SHAPE = (NUM_FRAMES, 3, IMAGE_SIZE, IMAGE_SIZE)
 BATCH_SIZE = 32
 
-
-INPUT_PATH = "s3://ray-data-eval-us-west-2/kinetics/k700-2020/train/abseiling/"
+if args.source == "local":
+    print("Using local data.")
+    INPUT_PATH = "/home/ubuntu/kinetics/kinetics/k700-2020/train"
+else:
+    print("Using S3 data.")
+    try:
+        with open("kinetics-train-10-percent.txt", "r") as f:
+            INPUT_PATH = eval(f.read())
+            print(len(INPUT_PATH))
+    except FileNotFoundError:
+        INPUT_PATH = download_train_directories(
+            bucket_name="ray-data-eval-us-west-2", prefix="kinetics/k700-2020/train/", percentage=10
+        )
 
 
 def timeit(name=None):
@@ -79,8 +105,9 @@ def preprocess_video(row: DataBatch) -> DataBatch:
     from decord import VideoReader, DECORDError
 
     global last_good_row
+
+    video_bytes = row["bytes"]
     try:
-        video_bytes = row["bytes"]
         vr = VideoReader(
             io.BytesIO(video_bytes),
             num_threads=1,
@@ -101,8 +128,10 @@ def preprocess_video(row: DataBatch) -> DataBatch:
     ret = processor(frames, return_tensors="np")
     arr = ret.data["pixel_values"]
     # time.sleep(1)
+
     if last_good_row is None:
         last_good_row = {"video": arr}
+
     return {"video": arr}
 
 
@@ -113,21 +142,49 @@ def collate_video_frames(batch: DataBatch) -> DataBatch:
 @timeit
 def main():
     classifier = Classifier()
+
+    ACCELERATOR = "NVIDIA_A10G"
+    TIMELINE_FILENAME = f"video_inference_{args.source}_{ACCELERATOR}_batch_{BATCH_SIZE}.json"
+    GPU_TIMELINE_FILENAME = (
+        f"video_inference_{args.source}_{ACCELERATOR}_batch_{BATCH_SIZE}_gpu.json"
+    )
+    CSV_FILENAME = f"video_inference_{args.source}_{ACCELERATOR}_batch_{BATCH_SIZE}.csv"
+    start_time = time.time()
+    rows_read = 0
+
+    # Initialize tput csv file writer
+    csv_file_obj = open(CSV_FILENAME, "w")
+    writer = csv.writer(csv_file_obj)
+    writer.writerow(["time_from_start", "number_of_rows_finished"])
+    writer.writerow([0, 0])
+
     ds = ray.data.read_binary_files(
         INPUT_PATH,
         override_num_blocks=1291,
     )
+
     ds = ds.map(preprocess_video)
 
+    tracer = ChromeTracer(GPU_TIMELINE_FILENAME, ACCELERATOR)
     last_batch_time = time.time()
     for batch in ds.iter_batches(batch_size=BATCH_SIZE, _collate_fn=collate_video_frames):
         print(f"Time to read batch: {time.time() - last_batch_time}")
-        with profiling.profile("Inference"):
+        # with profiling.profile("Inference"):
+        with tracer.profile("task:gpu_execution"):
             print(classifier(batch))
+
         last_batch_time = time.time()
 
+        # Write time_from_start, number_of_rows_finished to csv file
+        rows_read += len(batch["video"])
+        writer.writerow([last_batch_time - start_time, rows_read])
+
     print(ds.stats())
-    ray.timeline("video_inference.json")
+
+    # Save and combine cpu, gpu timeline view
+    tracer.save()
+    ray.timeline(TIMELINE_FILENAME)
+    append_gpu_timeline(TIMELINE_FILENAME, GPU_TIMELINE_FILENAME)
 
 
 if __name__ == "__main__":
