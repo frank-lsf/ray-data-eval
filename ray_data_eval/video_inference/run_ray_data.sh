1774721d03cc6f2f74302cab4c45bea7c6a4ea16
Ron Yifeng Wang <78146401+ronyw7@users.noreply.github.com>
2024-05-01 14:19:06 -0700

    Finish video inference scaling (#51)
    * Finish video inference scaling
    
    * Fix format

--- ray_data_eval/video_inference/run_ray_data.sh
+++ ray_data_eval/video_inference/run_ray_data.sh
@@ -1,2 +1,2 @@
-python ray_data_pipeline_map.py --source local > video_inference_local_g5_xlarge_batch_32.out 2>&1
+export RAY_DEDUP_LOGS=0 # Important to disable Ray log deduplication
 python ray_data_pipeline_map.py --source s3 > video_inference_s3_g5_xlarge_batch_32.out 2>&1
