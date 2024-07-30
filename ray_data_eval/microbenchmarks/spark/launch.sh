ec4d8478574009d2fc8adf7bfc9e457054df06ab
Amog Kamsetty <amogkam@users.noreply.github.com>
2024-07-30 13:43:06 -0700

    Update Spark benchmark and add stage level scheduling (#62)

--- ray_data_eval/microbenchmarks/spark/launch.sh
+++ ray_data_eval/microbenchmarks/spark/launch.sh
@@ -1,11 +1,12 @@
-export SPARK_EVENTS_PATH=/home/ubuntu/ray-data-eval/logs/spark-events
-export SPARK_TRACE_EVENT_PATH=/home/ubuntu/ray-data-eval/logs/spark-trace-events
+export SPARK_EVENTS_PATH=/home/ray/default/ray-data-eval/logs/spark-events
+export SPARK_TRACE_EVENT_PATH=/home/ray/default/ray-data-eval/logs/spark-trace-events
 export SPARK_EVENTS_FILEURL=file://$SPARK_EVENTS_PATH
-export SPARK_HOME=/home/ubuntu/miniconda3/envs/ray-data/lib/python3.10/site-packages/pyspark
-export PYSPARK_PYTHON=/home/ubuntu/miniconda3/envs/ray-data/bin/python
+export SPARK_HOME=./spark-3.5.1-bin-hadoop3
+export PYSPARK_PYTHON=/home/ray/anaconda3/bin/python
 export SPARK_HISTORY_OPTS="-Dspark.history.fs.logDirectory=$SPARK_EVENTS_FILEURL"
 
-/home/ubuntu/miniconda3/envs/ray-data/lib/python3.10/site-packages/pyspark/sbin/stop-history-server.sh
-/home/ubuntu/miniconda3/envs/ray-data/lib/python3.10/site-packages/pyspark/sbin/start-history-server.sh
-# Still OOM
-python producer_consumer_gpu.py --mem-limit 30
+# export SPARK_WORKER_OPTS="-Dspark.worker.resource.gpu.amount=4 -Dspark.worker.resource.gpu.discoveryScript=./gpu_discovery.sh -Dspark.executor.instances=8 -Dspark.executor.cores=1"
+
+./spark-3.5.1-bin-hadoop3/sbin/stop-history-server.sh
+./spark-3.5.1-bin-hadoop3/sbin/start-history-server.sh
+python producer_consumer_gpu.py --stage-level-scheduling
--- ray_data_eval/microbenchmarks/spark/producer_consumer_gpu.py
+++ ray_data_eval/microbenchmarks/spark/producer_consumer_gpu.py
@@ -1,7 +1,9 @@
 import time
 
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, IntegerType, BinaryType
+from pyspark import StorageLevel
+from pyspark.resource.requests import TaskResourceRequests
+from pyspark.resource import ResourceProfileBuilder
 import os
 import argparse
 
@@ -10,32 +12,44 @@ NUM_GPUS = 4
 
 MB = 1024 * 1024
 
-NUM_ROWS_PER_TASK = 10
-NUM_TASKS = 16 * 5
+NUM_ROWS_PER_TASK = 5
+NUM_TASKS = 16 * 5 * 2
 NUM_ROWS_TOTAL = NUM_ROWS_PER_TASK * NUM_TASKS
 ROW_SIZE = 100 * MB
 
 TIME_UNIT = 0.5
 
 
-def start_spark(executor_memory: int):
-    executor_memory_in_mb = int(executor_memory * 1024 / (NUM_CPUS + NUM_GPUS))
-    # https://spark.apache.org/docs/latest/configuration.html
-    spark = (
-        SparkSession.builder.appName("Local Spark Example")
-        .master(f"local[{NUM_CPUS + NUM_GPUS}]")
+def start_spark(stage_level_scheduling: bool):
+    spark_config = (
+        SparkSession.builder.config("spark.driver.host", "127.0.0.1").appName("Local Spark Example")
+        .master("spark://127.0.0.1:7077")
         .config("spark.eventLog.enabled", "true")
         .config("spark.eventLog.dir", os.getenv("SPARK_EVENTS_FILEURL"))
-        .config("spark.driver.memory", "2g")
-        # Amount of memory to use per executor process
-        .config("spark.executor.memory", f"{executor_memory_in_mb}m")
-        .config("spark.executor.instances", NUM_CPUS + NUM_GPUS)
-        # The number of cores to use on each executor.
-        .config("spark.executor.cores", 1)
-        # The maximum amount of CPU cores to request for the application from across the cluster (not from each machine).
-        .config("spark.cores.max", NUM_CPUS + NUM_GPUS)
-        .getOrCreate()
+        .config("spark.driver.bindAddress","127.0.0.1")
+        .config("spark.ui.enabled", "true")	
+        .config("spark.ui.port","4040")
     )
+    if not stage_level_scheduling:
+        spark_config = (
+            spark_config.config("spark.executor.cores", 1)
+            .config("spark.cores.max", NUM_CPUS)
+            .config("spark.executor.instances", NUM_CPUS)
+            # Allocate 1 GPU per executor and 1 GPU per task.
+            .config("spark.executor.resource.gpu.amount", 1)
+            .config("spark.task.resource.gpu.amount", 1)
+        )
+    else:
+        spark_config = (
+            spark_config.config("spark.dynamicAllocation.enabled", "false")
+            .config("spark.executor.instances", 4)
+            .config("spark.executor.cores", 2)
+            .config("spark.executor.memory", "4g")
+             # Allocate 1 GPU per executor.
+            .config("spark.executor.resource.gpu.amount", 1)
+        )
+    
+    spark = spark_config.getOrCreate()
     return spark
 
 
@@ -43,7 +57,8 @@ def producer_udf(row):
     time.sleep(TIME_UNIT * 10)
     for j in range(NUM_ROWS_PER_TASK):
         data = b"1" * ROW_SIZE
-        yield (data, row.item * NUM_ROWS_PER_TASK + j)
+        assert len(data) == ROW_SIZE
+        yield (data, row * NUM_ROWS_PER_TASK + j)
 
 
 def consumer_udf(row):
@@ -57,53 +72,71 @@ def inference_udf(row):
     return 1
 
 
-def run_spark_data(spark):
+def run_spark_data(spark, stage_level_scheduling: bool = False, cache: bool = False, cache_disk: bool = False):
     start = time.perf_counter()
 
-    items = [(item,) for item in range(NUM_TASKS)]
-    input_schema = StructType([StructField("item", IntegerType(), True)])
-    df = spark.createDataFrame(items, schema=input_schema)
+    if stage_level_scheduling:
+        # For the CPU stages, request 1 CPU and 0.5 GPU. This will run 8 concurrent tasks.
+        # cpu_task_requests = TaskResourceRequests().cpus(1).resource("gpu", 0.5)
+        # For the GPU stages, request 1 CPU and 1 GPU. This will run 4 concurrent tasks.
+        gpu_task_requests = TaskResourceRequests().cpus(1).resource("gpu", 1)
 
-    print("df.count()", df.count())
+        builder = ResourceProfileBuilder()
+        # cpu_task_profile = builder.require(cpu_task_requests).build
+        gpu_task_profile = builder.require(gpu_task_requests).build
+    
 
-    producer_schema = StructType(
-        [StructField("data", BinaryType(), True), StructField("id", IntegerType(), True)]
-    )
-    producer_rdd = df.rdd.flatMap(producer_udf)
-    producer_df = spark.createDataFrame(producer_rdd, schema=producer_schema)
+    rdd = spark.sparkContext.range(start=0, end=NUM_TASKS, numSlices=NUM_TASKS) # Set NUM_TASKS as the number of partitions.
 
-    print("producer_df.count()", producer_df.count())
+    producer_rdd = rdd.flatMap(producer_udf)
+    # if stage_level_scheduling:
+    #     producer_rdd = producer_rdd.withResources(cpu_task_profile)
+    if cache:
+        producer_rdd = producer_rdd.cache()
+    elif cache_disk:
+        producer_rdd = producer_rdd.persist(StorageLevel.MEMORY_AND_DISK)
+        print(producer_rdd.getStorageLevel())
 
-    consumer_schema = StructType([StructField("data", BinaryType(), True)])
-    consumer_rdd = producer_df.rdd.map(consumer_udf)
-    consumer_df = spark.createDataFrame(consumer_rdd, schema=consumer_schema)
+    
+    consumer_rdd = producer_rdd.map(consumer_udf)
+    # if stage_level_scheduling:
+    #     consumer_rdd = consumer_rdd.withResources(cpu_task_profile)
+    if cache:
+        consumer_rdd = consumer_rdd.cache()
+    elif cache_disk:
+        consumer_rdd = consumer_rdd.persist(StorageLevel.MEMORY_AND_DISK)
+        print(consumer_rdd.getStorageLevel())
+    
+    
+    if stage_level_scheduling:
+        # Call repartition to force a new stage for stage level scheduling
+        inference_rdd = consumer_rdd.repartition(NUM_TASKS)
+        inference_rdd = inference_rdd.map(lambda row: (inference_udf(row),)).withResources(gpu_task_profile)
+    else:
+        inference_rdd = consumer_rdd.map(lambda row: (inference_udf(row),))
 
-    print("consumer_df.count()", consumer_df.count())
-
-    result_schema = StructType([StructField("result", IntegerType(), True)])
-    inference_rdd = consumer_df.rdd.map(lambda row: (inference_udf(row),))
-    inference_df = spark.createDataFrame(inference_rdd, schema=result_schema)
-
-    print("inference_df.count()", inference_df.count())
-
-    total_processed = inference_df.agg({"result": "sum"}).collect()[0][0]
+    print("inference_df.count()", inference_rdd.count())
 
     run_time = time.perf_counter() - start
-    print(f"\nTotal length of data processed: {total_processed:,}")
     print(f"Run time: {run_time:.2f} seconds")
-    return total_processed
 
 
-def bench(mem_limit):
-    spark = start_spark(mem_limit)
-    run_spark_data(spark)
+def bench(stage_level_scheduling, cache, cache_disk):
+    spark = start_spark(stage_level_scheduling)
+    run_spark_data(spark, stage_level_scheduling, cache, cache_disk)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--mem-limit", type=int, required=False, help="Memory limit in GB", default=30
+        "--stage-level-scheduling", action="store_true", required=False, help="Whether to enable stage level scheduling", default=False
+    )
+    parser.add_argument(
+        "--cache", action="store_true", required=False, help="Whether to cache intermediate datasets in memory", default=False
+    )
+    parser.add_argument(
+        "--cache_disk", action="store_true", required=False, help="Whether to cache intermediate datasets in memory and disk", default=False
     )
     args = parser.parse_args()
 
-    bench(args.mem_limit)
+    bench(args.stage_level_scheduling, args.cache, args.cache_disk)
