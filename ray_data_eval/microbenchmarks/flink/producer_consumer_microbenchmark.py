d878b6750a27550d6160748e226d85ad3086d21e
Frank Luan <lsf@berkeley.edu>
2024-10-27 18:54:23 +0000

    Use busy loop for Flink

--- ray_data_eval/microbenchmarks/flink/producer_consumer_microbenchmark.py
+++ ray_data_eval/microbenchmarks/flink/producer_consumer_microbenchmark.py
@@ -8,9 +8,8 @@ import json
 import resource
 
 NUM_CPUS = 8
-PRODUCER_PARALLELISM = 1
-# CONSUMER_PARALLELISM = NUM_CPUS - PRODUCER_PARALLELISM
-CONSUMER_PARALLELISM = 1
+PRODUCER_PARALLELISM = None
+CONSUMER_PARALLELISM = None
 EXECUTION_MODE = "process"
 MB = 1024 * 1024
 GB = 1024 * MB
@@ -21,18 +20,29 @@ TIME_UNIT = 0.5
 
 NUM_ROWS_PER_PRODUCER = 1000
 NUM_ROWS_PER_CONSUMER = 100
-MEMORY_USAGE_CURRENT_PROGRAM =  4 * GB
+MEMORY_USAGE_CURRENT_PROGRAM = 4 * GB
+
 
 def configure_flink_memory(env: StreamExecutionEnvironment, config_path: str):
     config = Configuration()
     config.load_yaml_file(config_path)
     env.set_configuration(config)
 
+
 def limit_memory(max_mem):
-    """ Set a memory limit in bytes. """
+    """Set a memory limit in bytes."""
     soft, hard = resource.getrlimit(resource.RLIMIT_AS)
     resource.setrlimit(resource.RLIMIT_AS, (max_mem, hard))
-    
+
+
+def busy_loop_for_seconds(time_diff):
+    start = time.perf_counter()
+    i = 0
+    while time.perf_counter() - start < time_diff:
+        i += 1
+        continue
+
+
 class Producer(FlatMapFunction):
     def open(self, runtime_context: RuntimeContext):
         self.task_info = runtime_context.get_task_name_with_subtasks()
@@ -41,7 +51,7 @@ class Producer(FlatMapFunction):
     def flat_map(self, value):
         producer_start = time.time()
         # print("Producer", value)
-        time.sleep(TIME_UNIT * 10)
+        busy_loop_for_seconds(TIME_UNIT * 10)
         producer_end = time.time()
         log = {
             "cat": "producer:" + str(self.task_index),
@@ -62,7 +72,7 @@ class Producer(FlatMapFunction):
 # class Consumer(MapFunction):
 #     def map(self, items):
 #         print("Consumer", len(items))
-#         time.sleep(TIME_UNIT / NUM_ROWS_PER_CONSUMER)
+#         busy_loop_for_seconds(TIME_UNIT / NUM_ROWS_PER_CONSUMER)
 #         return len(items)
 
 
@@ -81,7 +91,7 @@ class ConsumerActor(ProcessFunction):
             self.consumer_start = time.time()
 
         # print("Consumer", self.idx, len(self.current_batch), len(value))
-        time.sleep(TIME_UNIT / NUM_ROWS_PER_CONSUMER)
+        busy_loop_for_seconds(TIME_UNIT / NUM_ROWS_PER_CONSUMER)
         self.current_batch.append(value)
         self.idx += 1
         if len(self.current_batch) < NUM_ROWS_PER_CONSUMER:
@@ -110,18 +120,14 @@ def run_flink(env):
     ds = env.from_collection(items, type_info=Types.INT())
 
     producer = Producer()
-    # consumer = Consumer()
+    consumer = ConsumerActor()
 
-    ds = ds.flat_map(producer, output_type=Types.PICKLED_BYTE_ARRAY()).set_parallelism(
-        PRODUCER_PARALLELISM
-    )
-
-    ds = (
-        # ds.map(consumer, output_type=Types.LONG())
-        # .set_parallelism(CONSUMER_PARALLELISM)
-        # .disable_chaining()
-        ds.process(ConsumerActor()).set_parallelism(CONSUMER_PARALLELISM)
-    )
+    ds = ds.flat_map(producer, output_type=Types.PICKLED_BYTE_ARRAY())
+    if PRODUCER_PARALLELISM is not None:
+        ds = ds.set_parallelism(PRODUCER_PARALLELISM)
+    ds = ds.process(consumer)
+    if CONSUMER_PARALLELISM is not None:
+        ds = ds.set_parallelism(CONSUMER_PARALLELISM)
 
     result = []
     for length in ds.execute_and_collect():
@@ -138,17 +144,25 @@ def run_flink(env):
 def run_experiment():
     config = Configuration()
     config.set_string("python.execution-mode", EXECUTION_MODE)
-    
+
     # Lower the memory so that it might possibly fit under 2GB
     # config.set_string("taskmanager.memory.process.size", "1600m")
     # config.set_string("taskmanager.memory.flink.size", "1200m")
     # config.set_string("taskmanager.memory.jvm-overhead.size", "200m")
     # config.set_string("taskmanager.memory.managed.size", "100m")
     # config.set_string("taskmanager.memory.network.size", "100m")
-    
+
+    # Adaptive parallelism
+    if PRODUCER_PARALLELISM is None and CONSUMER_PARALLELISM is None:
+        config.set_string("jobmanager.scheduler", "Adaptive")
+        config.set_integer("jobmanager.adaptive-scheduler.max-parallelism-increase", NUM_CPUS)
+        config.set_integer("jobmanager.adaptive-scheduler.initial-parallelism", 1)
+        config.set_integer("jobmanager.adaptive-scheduler.min-parallelism", 1)
+        config.set_integer("jobmanager.adaptive-scheduler.max-parallelism", NUM_CPUS)
+
     env = StreamExecutionEnvironment.get_execution_environment(config)
 
-    limit_memory(MEMORY_USAGE_CURRENT_PROGRAM)
+    # limit_memory(MEMORY_USAGE_CURRENT_PROGRAM)
     run_flink(env)
 
 
